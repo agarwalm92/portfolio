6dc46e569f2583876a768887877d07c164413949
Mayank <agarwalm92@gmail.com>
2024-04-19 00:39:18 +0530

    Added corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import { ThemeProvider } from "@mui/material";
+import { darkTheme } from "./utils/Themes";
+import { useState } from "react";
+import AppAppBar from "./components/AppAppBar";
+
 function App() {
+  const [mode, setMode] = useState("light");
+  const [showCustomTheme, setShowCustomTheme] = useState(true);
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === "dark" ? "light" : "dark"));
+  };
+  const toggleCustomTheme = () => {
+    setShowCustomTheme((prev) => !prev);
+  };
+
   return (
-    <>
-      <p>portfolio</p>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <AppAppBar mode={mode} />
+    </ThemeProvider>
   );
 }
 
